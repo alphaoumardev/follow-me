b7893802aeb56ba3c1a32b74ebdb02bc3deda6ee
oumar <oumardialo98@qq.com>
2022-01-01 20:42:59 +0800

    pins

--- client/src/components/Pin.jsx
+++ client/src/components/Pin.jsx
@@ -67,10 +67,7 @@ const Pin = ({ pin }) => {
                 className=" relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out">
                     <img className="rounded-lg w-full " src={(urlFor(image).width(250).url())} alt="user-post" />
                 {postHovered && (
-                    <div
-                        className="absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50"
-                        style={{ height: '100%' }}
-                    >
+                    <div  style={{ height: '100%' }} className="absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50" >
                         <div className="flex items-center justify-between">
                             <div className="flex gap-2">
                                 <a
